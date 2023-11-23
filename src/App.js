6c9775c115e52b5499055eac64a290a4bb111f3d
shivapabbu1 <93253969+shivapabbu1@users.noreply.github.com>
2023-11-23 15:23:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,18 +3,34 @@ import './App.css';
 import Mycomp from './Components/Mycomp.js';
 import Album from './Nov-22-Components/Album.jsx';
 import Mapcomp from './Nov-22-Components/Mapcomp.jsx';
+import Counter from './Nov-23-Components.jsx/Counter.jsx';
+import MovieTable from './Nov-23-Components.jsx/MovieTable.jsx';
 function App() {
   return (
     <>
     
     <h1 style={{textAlign:"center"}}>  I am app  page</h1>
+   
+    <div>
     <h1 style={{textAlign:"center"}}>DAY-1</h1>
     <Mycomp/>
-    <hr></hr>
-   <h1 style={{textAlign:"center"}}>Day:2</h1>
-    <Mapcomp/>
+    </div>
+
+    <h1 style={{textAlign:"center"}}>Day:2</h1>
+    
+    <div>
+   <Mapcomp/>
     <hr></hr>
     <Album/>
+    </div>
+
+    <h1 style={{textAlign:"center"}}>Day:3</h1>
+   
+    <div>
+   <Counter/>
+   <br/>
+   <MovieTable/>
+    </div>
     
     </>
     
@@ -22,3 +38,4 @@ function App() {
 }
 
 export default App;
+
