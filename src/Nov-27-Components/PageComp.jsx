f17d704e41ab9e80a5fde712c8579a9d656829a4
shivapabbu1 <93253969+shivapabbu1@users.noreply.github.com>
2023-11-28 13:09:05 +0530

    Update PageComp.jsx(nov-27 && nov-28)

--- src/Nov-27-Components/PageComp.jsx
+++ src/Nov-27-Components/PageComp.jsx
@@ -1,13 +1,29 @@
 import React, { Component } from 'react'
 import Datastu from "./Data.jsx"
 import Pagenation from './Pagenation.jsx'
+import _ from 'lodash'
 export default class PageComp extends Component {
     state={
         stu:Datastu,
+       currentPage:1,
         perPage:4,
+     
 
     }
+    pageChange=(d)=>{
+      console.log("cilcked"+d);
+      this.setState({currentPage:d})
+   }
+   pageFilter(){
+    const startIndex=(this.state.currentPage-1)*this.state.perPage;
+    return _(this.state.stu).slice(startIndex).take(this.state.perPage).value();
+   }
+  
+
   render() {
+   
+   const filtredStu=this.pageFilter()
+   console.log(this.pageFilter());
     return (
      <>
      <table className="table table-striped">
@@ -21,20 +37,22 @@ export default class PageComp extends Component {
             </tr>
           </thead>
           <tbody>
-            {this.state.stu.map((each)=>{
+         
+            {this.pageFilter().map(each=>{
                 return(
+                  
                     <tr key={each.roll_no}>
                         <td>{each.name}</td>
                         <td>{each.roll_no}</td>
                         <td>{each.college_name}</td>
                     </tr>
                 )})}
+                
           </tbody>
         </table>
-        <Pagenation totalStudent={this.state.stu.length} studentPerPage={this.state.perPage}/>
+        <Pagenation totalStudent={this.state.stu.length} studentPerPage={this.state.perPage} pageChange={this.pageChange }/>
      
      </>
     )
   }
 }
-
